90a209c05857edc26878b59ad5310b0bd28b3586
daulet-bulegenov <dallas92@outlook.com>
2025-03-03 00:05:48 +0500

    init commit

--- src/main/java/com/example/spribe/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/spribe/exception/GlobalExceptionHandler.java
@@ -2,13 +2,11 @@ package com.example.spribe.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.util.Map;
 
-@RestControllerAdvice
+//@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ConflictException.class)
